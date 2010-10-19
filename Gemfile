f300e9b599b33e7027b56d9ae14166a76ecf88bc
Chris Boertien <chris.boertien@gmail.com>
2010-10-19 01:53:58 -0500

    Initial rails app
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'rspec-rails', '>= 2.0.0.beta.22'
+  gem 'webrat'
+end
+
+group :development do
+  gem 'faker', '0.3.1'
+  gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tzm::Application.config.secret_token = '2ff5d1f549d74248cf59d4722c7057478caa0c43c29ae9ef9181daedd4249d435e851abe52c433dfbe12beccfca61314e75f591f1823e99fb98dad1f517f763b'
