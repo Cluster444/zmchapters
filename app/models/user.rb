8184d329bd12500ef1a2d29dfecf3cedc685ebb0
Chris Boertien <chris.boertien@gmail.com>
2010-10-28 15:52:52 -0500

    removed chapter_id validation from user and fixed errors in country select box dropdown
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :chapter_id, :presence => true
-
   belongs_to :chapter, :counter_cache => true
   belongs_to :country, :counter_cache => true
 end
