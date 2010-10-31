e52824a3c450927d1b84b93ac96abb753898f4c9
Chris Boertien <chris.boertien@gmail.com>
2010-10-31 15:19:06 -0500

    dont need attr_accessor with devise, bugs the passwords
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
   #FIXME - Probably shouldn't expose chapter_id like this...
   attr_accessible :name, :alias, :email, :password, :password_confirmation, :chapter_id, :roles_mask, :country_id, :roles
 
