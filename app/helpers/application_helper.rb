df5450c8962ae04e56de12bb74a9eb9dc1d44a27
Chris Boertien <chris.boertien@gmail.com>
2010-11-22 16:38:43 -0600

    refactor some helper methods out of the garbage can application_helper
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,4 @@
 module ApplicationHelper
-  
-  def title(page_title, show_title = true)
-    content_for(:title) { page_title.to_s }
-    @show_title = show_title
-  end
-
-  def show_title?
-    @show_title
-  end
-
-  def css(*files)
-    content_for(:css) { stylesheet_link_tag(*files) }
-  end
-
-  def js(*files)
-    content_for(:js) { javascript_include_tag(*files) }
-  end
-
-  def page_class(classes)
-    content_for(:page_class) { classes.to_s }
-  end
-
-  def page_head_class(classes)
-    content_for(:page_head_class) { classes }
-  end
-
-  def simple_page_head(content)
-    content_for(:page_head, content_tag(:h1, content))
-  end
-
   def edit_link(object, content = "Edit")
     link_to(content, [:edit, object]) if can? :update, object
   end
