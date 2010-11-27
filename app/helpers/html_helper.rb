6790249fdbaea7ba7e629305fb6974c7d8f7ee70
Chris Boertien <chris.boertien@gmail.com>
2010-11-27 13:42:46 -0600

    fix argument ordering, hashes need to be after regular args
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -3,7 +3,7 @@ module HtmlHelper
     ListRenderer.new(self, block).to_ul
   end
 
-  def ul_link_to(opts={}, collection)
+  def ul_link_to(collection, opts={})
     ul opts do |list|
       collection.each do |item|
         list.link_to(item.name, geo_path(item))
